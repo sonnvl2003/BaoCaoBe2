9dee8acfd3824644a7475bd5b45ff66144e5cbce
sonnvl2003 <21211tt2356@mail.tdc.edu.vn>
2023-04-12 21:14:27 +0700

    update migration user ( phone , image)

--- app/database/migrations/2014_10_12_000000_create_users_table.php
+++ app/database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,7 +15,9 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
+            $table->string('name');            
+            $table->string('image');            
+            $table->integer('phone');            
             $table->string('email',100)->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
