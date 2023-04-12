9a747890f055ef5afb07b662d7bec89cefe97a12
hanh42 <91781354+hanh42@users.noreply.github.com>
2023-04-12 23:38:17 +0700

    4/12/2023 11:37 create register and ui of register

--- app/app/Http/Controllers/CustomAuthController.php
+++ app/app/Http/Controllers/CustomAuthController.php
@@ -1,4 +1,5 @@
 <?php
+
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
@@ -18,35 +19,54 @@ class CustomAuthController extends Controller
         return view('auth.login');
     }
 
-    public function customLogin(Request $request)
-    {
-        $request->validate([
-            'email' => 'required',
-            'password' => 'required',
-        ]);
-
-        $credentials = $request->only('email', 'password');
-        if (Auth::attempt($credentials)) {
-            return redirect()->intended('dashboard')
-                ->withSuccess('Signed in');
-        }
-
-        return redirect("login")->withSuccess('Login details are not valid');
-    }
 
     public function dashboard()
     {
-        if(Auth::check()){
+        if (Auth::check()) {
             return view('dashboard');
         }
 
         return redirect("login")->withSuccess('You are not allowed to access');
     }
 
-    public function signOut() {
-        SessionSession::flush();
-        Auth::logout();
-
-        return Redirect('login');
+    public function registration()
+    {
+        return view('auth.registration');
     }
+
+    public function customRegistration(Request $request)
+    {
+        $request->validate([
+            'name' => 'required',
+            'email' => 'required|email|unique:users',
+            'password' => 'required|min:6',
+            'phone' => 'required | numeric | digits:10',
+            'image' => 'required'
+        ]);
+
+        $data = $request->all();
+        $data['image'] = $this->createNewName($request);
+        $check = $this->create($data);
+        return redirect("dashboard")->withSuccess('You have signed-in');
+    }
+
+    public function createNewName(Request $Request)
+    {
+        $newNameImg = "IMG" . time() . '.' . $Request->file('image')->guessExtension();
+        $Request->image->move(base_path('./public/image'), $newNameImg);
+        return $newNameImg;
+    }
+
+
+    public function create(array $data)
+    {
+        return User::create([
+            'name' => $data['name'],
+            'email' => $data['email'],
+            'password' => FacadesHash::make($data['password']),
+            'phone' => $data['phone'],
+            'image' => $data['image']
+        ]);
+    }
+
 }
